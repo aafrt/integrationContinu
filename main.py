f9e5d1968f881bd9dc76f65c6eb17054c61471bc
unknown <afrahatri1993@gmail.comgit config --global user.name aafrtgit commit -m first>
2024-04-04 12:16:26 +0200

    generation

---
+++ main.py
@@ -0,0 +1,7 @@
+fichier = open("index.html","w")
+
+fichier.write("<html><body><h1>Site web génére </h1>")
+fichier.write("<p>Ceci est le site du client <b>client</p>")
+fichier.write("</body></html>")
+
+fichier.close()
